75b08bca6a617034001531ad0b075eb1daff0daa
matanki.saito <matanki.saito@gmail.com>
2020-07-29 21:02:10 +0900

    クレデンシャルをダウンロードする方式に変更

--- decrypt_credential.py
+++
@@ -1,8 +0,0 @@
-import sys
-import getpass
-import my_aes
-import os
-
-password = os.environ.get("CREDENTIALS_PASSWORD")
-dec = my_aes.decrypt(sys.stdin.buffer.read(), password)
-sys.stdout.buffer.write(dec)
